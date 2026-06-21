43d2eeae98954666af6d6468873fec5e82678621
Murtaza Jafari <65913750+jafarimurtaza@users.noreply.github.com>
2026-06-21 17:02:31 +0300

    First commit on the page/home branch

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html data-theme="night" lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <div>Footer</div>
+      </body>
     </html>
   );
 }
