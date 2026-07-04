ecc2bf135981fd34b4bf1709efdfbcc915784ba2
shukria <shukria.tech@gmail.com>
2026-07-04 09:05:18 +0430

    started with the impact page also created components

---
+++ src/components/impact/ImpactHeroSection.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+export default function ImpactHeroSection() {
+  return (
+    <div>
+        <span>IMPACT</span>
+        <h1>Measuring What Matters</h1>
+        <p>Afghan Geeks is more than an education programme—it is a statement about who
+          <br />
+        gets to build the future.</p>
+    </div>
+  );
+}
---
+++ src/components/impact/ImpactMain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ImpactHeroSection from './ImpactHeroSection'
+export default function ImpactMain() {
+  return (
+    <div>
+        <ImpactHeroSection />
+    </div>
+  )
+}
