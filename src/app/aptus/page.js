3fdaea12edba9f69b5f90beca6bb164d9abb9bcc
zarifa1401 <zarifarahmani99@gmail.com>
2026-07-05 00:38:24 +0430

    started with aptus page and aptus main component

---
+++ src/app/aptus/page.js
@@ -0,0 +1,6 @@
+import AptusMain from "@/components/aptus/AptusMain";
+
+export default function AptusPage(){
+    return(<AptusMain />)
+
+}
---
+++ src/components/aptus/AptusMain.jsx
@@ -0,0 +1,5 @@
+export default function AptusMain(){
+    return(
+        <div><h1>Aptus page</h1></div>
+    )
+}
