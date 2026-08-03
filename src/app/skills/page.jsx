3be0ef608d4af42fd6456f3504354d5f3b618cbc
Mahfoza <130636225+Sultani2023@users.noreply.github.com>
2026-08-04 01:56:38 +0430

    feat(skills): improve responsive graph by hiding edges on mobile and tablet

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -2,7 +2,7 @@ import SkillsGraph from "../../components/skills/SkillsGraph";
 
 export default function SkillPage() {
   return (
-  <main className="h-screen w-full overflow-hidden bg-primary">
+  <main className="h-screen w-full overflow-hidden bg-base-200 text-base-content">
  
 
       <div className="h-[calc(100vh-100px)] w-full">
