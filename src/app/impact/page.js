0dae9e0602c79c6ee8cf55f7d627cea2e1b8e917
rahimishukria <shukria.tech@gmail.com>
2026-08-22 11:36:14 +0430

    metadata added to the page.js .

--- src/app/impact/page.js
+++ src/app/impact/page.js
@@ -1,7 +1,11 @@
-
 import ImpactMain from "@/components/impact/ImpactMain";
+
+export const metadata = {
+  title: "Impact | Afghan Geeks",
+  description:
+    "Afghan Geeks trains the next generation of tech talent through hands-on education, mentorship, and real-world project experience.",
+};
+
 export default function Impact() {
-  return (
-      <ImpactMain />
-  )
+  return <ImpactMain />;
 }
